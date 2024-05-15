a1ae2511c6a8c0d32bb20036064e9084e55e0777
Your Name <you@example.com>
2024-05-14 22:00:04 -0700

    shohidul-commit

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,16 +1,17 @@
+'use client';
 import React from 'react';
 import LoginFrom from '../../components/LoginFrom';
-// import { redirect } from 'next/navigation';
-// import { getServerSession } from 'next-auth';
-// import authOptions from "../../app/api/auth/[...nextauth]/route"
+import { useSession } from 'next-auth/react';
+import { redirect } from 'next/navigation';
 
 
-const Login = async () => {
-  // const session = await getServerSession(authOptions);
 
-  // if (session) {
-  //   redirect("/blog");
-  // }
+const Login =  () => {
+  const { data: session } = useSession();
+
+  if (session) {
+    redirect("/blog");
+  }
 
   return (
     <div>
--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -1,19 +1,17 @@
 
+'use client';
+import { useSession } from 'next-auth/react';
 import SignupFrom from '../../components/SignupFrom';
-// import { redirect } from 'next/navigation';
-// import { getServerSession } from 'next-auth';
-// import authOptions from "../../app/api/auth/[...nextauth]/route"
+import { redirect } from 'next/navigation';
 
 
 
+const Signup = () => {
+  const { data: session } = useSession();
 
-const Signup = async () => {
-  // const session = await getServerSession(authOptions);
-
-  // if (session) {
-  //   redirect("/blog");
-  // }
-
+  if (session) {
+    redirect("/blog");
+  }
   return (
     <div>
       <SignupFrom />
